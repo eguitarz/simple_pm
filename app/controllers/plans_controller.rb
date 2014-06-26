cf9f30dec97a77421e05ef29620882e713016d57
eguitarz <dalema22@gmail.com>
2014-06-25 23:58:43 -0600

    Supported ajax plan list

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -2,5 +2,10 @@ class PlansController < ApplicationController
 	def show
 		@plan = Plan.find params[:id]
 		@tasks = @plan.tasks
+
+		respond_to do |format|
+			format.html
+			format.js
+		end
 	end
 end
