8075ed7c15be244f0700d50c97e813ef85f80276
eguitarz <dalema22@gmail.com>
2014-06-25 21:42:49 -0600

    Controllers and views for plan and task

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,2 +1,6 @@
 class PlansController < ApplicationController
+	def show
+		@plan = Plan.find params[:id]
+		@tasks = @plan.tasks
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ SimplePm::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'projects#index'
+  resources :plans
+  resources :tasks
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
